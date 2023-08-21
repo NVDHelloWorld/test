58bb63f00db578b219d66409dc7e4b3cc2966a6b
NVDHelloWorld <119719194+NVDHelloWorld@users.noreply.github.com>
2023-08-21 20:50:38 +0530

    socialmedia1.py

--- socialmedia1.py
+++ socialmedia1.py
@@ -3,45 +3,52 @@ from textblob import TextBlob
 import matplotlib.pyplot as plt
 import streamlit as st
 
-# Load the CSV dataset into a Pandas DataFrame
-csv_file_path = 'socialmedia.csv'
-df = pd.read_csv(r'C:\Users\Hp\Downloads\socialmedia.csv')
-
-# Data Preprocessing
-df['User Description 1'] = df['User Description 1'].str.lower()
-df['User Description 2'] = df['User Description 2'].str.lower()
-df['Post Text'] = df['Post Text'].str.lower()
-
 # Sentiment Analysis using TextBlob
 def get_sentiment(text):
     analysis = TextBlob(text)
     return analysis.sentiment.polarity
 
-df['sentiment'] = df['Post Text'].apply(get_sentiment)
-
-# User Engagement Analysis
-numerical_engagement_columns = ['User ID', 'Likes/Reactions', 'Shares/Retweets', 'Comments']
-df['total_engagement'] = df[numerical_engagement_columns].sum(axis=1)
-
 # Streamlit App
 st.title('Social Media Analysis')
 
-# Sentiment Analysis Visualization
-st.subheader('Sentiment Analysis')
-plt.figure(figsize=(8, 5))
-df['sentiment'].hist(bins=20, color='blue', alpha=0.7)
-plt.title('Sentiment Analysis')
-plt.xlabel('Sentiment Polarity')
-plt.ylabel('Frequency')
-st.pyplot()
-st.set_option('deprecation.showPyplotGlobalUse', False)
+# File Upload
+uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
 
-# User Engagement Analysis Visualization
-st.subheader('User Engagement Analysis')
-plt.figure(figsize=(8, 5))
-df.plot(y='total_engagement', kind='line', color='green', marker='o')
-plt.title('User Engagement Analysis')
-plt.ylabel('Total Engagement')
-st.pyplot()
-st.set_option('deprecation.showPyplotGlobalUse', False)
+if uploaded_file is not None:
+    # Load the CSV dataset into a Pandas DataFrame
+    df = pd.read_csv(uploaded_file)
 
+    # Data Preprocessing
+    df['Post Text'] = df['Post Text'].str.lower()
+
+    # Sentiment Analysis
+    df['sentiment'] = df['Post Text'].apply(get_sentiment)
+
+    # User Engagement Analysis
+    numerical_engagement_columns = ['User ID', 'Likes/Reactions', 'Shares/Retweets', 'Comments']
+    df['total_engagement'] = df[numerical_engagement_columns].sum(axis=1)
+
+    # Interactive Filtering
+    sentiment_range = st.slider("Select Sentiment Polarity Range", -1.0, 1.0, (-1.0, 1.0))
+    filtered_df = df[(df['sentiment'] >= sentiment_range[0]) & (df['sentiment'] <= sentiment_range[1])]
+
+    # Display Data Table
+    st.subheader('Filtered Data')
+    st.write(filtered_df)
+
+    # Sentiment Analysis Visualization
+    st.subheader('Sentiment Analysis')
+    plt.figure(figsize=(8, 5))
+    filtered_df['sentiment'].hist(bins=20, color='blue', alpha=0.7)
+    plt.title('Sentiment Analysis')
+    plt.xlabel('Sentiment Polarity')
+    plt.ylabel('Frequency')
+    st.pyplot()
+
+    # User Engagement Analysis Visualization
+    st.subheader('User Engagement Analysis')
+    plt.figure(figsize=(8, 5))
+    filtered_df.plot(y='total_engagement', kind='line', color='green', marker='o')
+    plt.title('User Engagement Analysis')
+    plt.ylabel('Total Engagement')
+    st.pyplot()
